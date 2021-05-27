07b412bba470dc8e87cf467c1b8f637da5986e50
msabat98 <mariasabatini-@hotmail.co.uk>
2021-05-27 13:17:10 +0100

    Create helperFunction.js

---
+++ helperFunction.js
@@ -0,0 +1,31 @@
+// Formats response to look presentable on webpage
+const renderResponse = (res) => {
+  // handles if res is falsey
+  if(!res){
+    console.log(res.status)
+  }
+  // in case res comes back as a blank array
+  if(!res.length){
+    responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>"
+    return
+  }
+
+  // creating an array to contain the HTML strings
+  let wordList = []
+  // looping through the response and maxxing out at 10
+  for(let i = 0; i < Math.min(res.length, 10); i++){
+    // creating a list of words
+    wordList.push(`<li>${res[i].word}</li>`)
+  }
+  // joins the array of HTML strings into one string
+  wordList = wordList.join("")
+
+  // manipulates responseField to render the modified response
+  responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`
+  return
+}
+
+// Renders response before it is modified
+const renderRawResponse = (res) => {
+  // taking the first 10 words from res
+  let trimmedResponse = res.slice(0
